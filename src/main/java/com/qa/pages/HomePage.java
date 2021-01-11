4164ec5ec31df3a7a2d957f26f20fe51f7af010b
unknown <sheshuriya99@gmail.com>
2021-01-11 16:25:57 +0530

    first git commit for my selenium code

---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,7 @@
+package com.qa.pages;
+
+public class HomePage {
+	public void Home() {
+			System.out.println("Home Page");
+		}
+}
---
+++ src/main/java/com/qa/pages/TestBase.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class TestBase {
+
+	public void test() {
+		System.out.println("Test method");
+	}
+}
